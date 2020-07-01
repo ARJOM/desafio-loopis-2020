91b54d438392287d84d83e26a44dc4cd8b9463f4
Ricart <ricarteusuo11@gmail.com>
2020-07-01 18:46:20 -0300

    feat: adiciona funcionalidade de cadastro de pessoa

--- js/friends.js
+++ js/friends.js
@@ -1,3 +1,29 @@
+// Verificando informações iniciais
+
+if (getObjectLocalStorage('users') === null){
+    setObjectLocalStorage('users', []);
+}
+
+
+// Crud de pessoas
+function create(){
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+
+    if (name=="" || email==""){
+        alert('informe valores válidos')
+    } else {
+        const user = {name, email};
+        
+        let users = getObjectLocalStorage('users');
+        users.push(user);
+        setObjectLocalStorage('users', users);
+
+        alert('usuário criado com sucesso')
+    }
+}
+
+
 // Manipulação de local storage
 
 function setObjectLocalStorage(key,value){
