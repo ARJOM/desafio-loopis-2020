c09091397e2197cff52841be79b096591d786d08
Ricart <ricarteusuo11@gmail.com>
2020-07-02 20:08:41 -0300

    feat: adiciona funcionalidade de sortear nomes de amigo secreto

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function secretFriend(){
+    let users = getObjectLocalStorage('users');
+    let choose = [];
+    users.forEach(user => {
+        let invalid = true;
+        let userId = users.indexOf(user);
+        while (invalid){
+            let id = Math.floor(Math.random() * users.length);
+            if (id!==userId){
+                choose.push(id);
+                user.friend = users[id].name;
+                users[userId] = user;
+                invalid = false;
+            }
+        }
+    });
+    setObjectLocalStorage('users', users);
+    alert("Sorteado");
+}
